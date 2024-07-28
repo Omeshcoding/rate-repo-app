c055e6f85f19ea63928a51051e39516763ddeac6
umesh <umesharma.us11@gmail.com>
2024-07-28 20:31:12 +0530

    Created RepositoryList and RepositoryItem component and rendered the FlatList component to render a single item on the repositories list

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  separator: {
+    marginTop: 10,
+  },
+});
+const RepositoryItem = ({
+  title,
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazersCount,
+  ratingAverage,
+  reviewCount,
+  ownerAvatarUrl,
+}) => {
+  return (
+    <View style={styles.separator}>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Review: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
