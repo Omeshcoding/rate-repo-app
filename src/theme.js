cefa1f7b1a75f5890edcf8755d49c50532ad9494
umesh <umesharma.us11@gmail.com>
2024-10-07 23:21:44 +0530

    Added Platform specific font family to the app

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,25 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
     textSecondary: '#586069',
     primary: '#0366d6',
     textWhite: '#fff',
-    primaryBackGround: '#e1e4e8 ',
+    primaryBackGround: '#e1e4e8',
   },
   fontSizes: {
     body: 14,
     subheading: 16,
     tabTitle: 20,
   },
+
   fonts: {
-    main: 'System',
+    fontFamily: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
   },
   fontWeights: {
     normal: '400',
