0c5b77c2538ecde54162261373f07fad4f0efe9a
umesh <umesharma.us11@gmail.com>
2024-10-08 09:30:29 +0530

    Added Platform specific font family to the app

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,10 @@
-import { Platform } from 'react-native';
-
 const theme = {
   colors: {
     textPrimary: '#24292e',
     textSecondary: '#586069',
     primary: '#0366d6',
     textWhite: '#fff',
-    primaryBackGround: '#e1e4e8',
+    primaryBackGround: '#e1e4e8 ',
   },
   fontSizes: {
     body: 14,
