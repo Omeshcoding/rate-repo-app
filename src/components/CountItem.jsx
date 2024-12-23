30ed93b48a30228c30e15004bb537dd0f5aaaf55
umesh <umesharma.us11@gmail.com>
2024-12-23 22:31:48 +0530

    Added a function to format the stats

--- src/components/CountItem.jsx
+++ src/components/CountItem.jsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native';
+import formatInThousands from '../utils/formatInThousands';
 
 export default CountItem = ({ styles, label, count }) => (
   <View style={styles.countItem}>
     <Text style={styles.countItemCount} fontWeight="bold">
-      {count}
+      {formatInThousands(count)}
     </Text>
     <Text color="textSecondary">{label} </Text>
   </View>
